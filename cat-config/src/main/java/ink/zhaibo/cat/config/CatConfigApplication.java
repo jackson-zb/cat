1bf71ef6342d5c57c4fd3d44ca9f74f16fd391eb
zhaibo <729963917@qq.com>
2018-09-08 18:02:10 +0800

    使用consul作为服务注册中心,service-one注册到consul.

--- cat-config/src/main/java/ink/zhaibo/cat/config/CatConfigApplication.java
+++ cat-config/src/main/java/ink/zhaibo/cat/config/CatConfigApplication.java
@@ -1,4 +1,4 @@
-package ink.zhaibo.cat;
+package ink.zhaibo.cat.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- cat-config/src/test/java/ink/zhaibo/cat/config/CatConfigApplicationTests.java
+++ cat-config/src/test/java/ink/zhaibo/cat/config/CatConfigApplicationTests.java
@@ -1,4 +1,4 @@
-package ink.zhaibo.cat;
+package ink.zhaibo.cat.config;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
