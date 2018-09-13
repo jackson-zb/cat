745a01f63527da52ad3b89823f1d456ab8f6316a
zhaibo <729963917@qq.com>
2018-09-13 18:20:45 +0800

    创建可视化监控目录.

--- cat-services/cat-service-one/src/main/java/ink/zhaibo/cat/service/one/CatServiceOneApplication.java
+++ cat-services/cat-service-one/src/main/java/ink/zhaibo/cat/service/one/CatServiceOneApplication.java
@@ -1,22 +1,13 @@
 package ink.zhaibo.cat.service.one;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
 
-/**
- * @Author zhaibo
- * @Date 2018/9/7
- * @Description Service One
- */
 @SpringBootApplication
 @EnableDiscoveryClient
 public class CatServiceOneApplication {
     public static void main(String[] args) {
-        SpringApplication.run(CatServiceOneApplication.class,args);
+        SpringApplication.run(CatServiceOneApplication.class, args);
     }
 }
