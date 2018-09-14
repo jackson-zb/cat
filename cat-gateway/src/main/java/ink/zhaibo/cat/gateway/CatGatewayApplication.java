f119428b086ed4629e6d9c013ebf6315f81116fc
zhaibo <729963917@qq.com>
2018-09-14 18:53:04 +0800

    create api gateway.

---
+++ cat-gateway/src/main/java/ink/zhaibo/cat/gateway/CatGatewayApplication.java
@@ -0,0 +1,12 @@
+package ink.zhaibo.cat.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatGatewayApplication.class, args);
+	}
+}
