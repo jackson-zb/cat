611eb0cc41ad3d27966433e80e67d09cb4a88f76
zhaibo <729963917@qq.com>
2018-09-30 16:16:52 +0800

    update srping cloud zuul.

--- cat-zuul/src/main/java/ink/zhaibo/cat/zuul/CatZuulApplication.java
+++ cat-zuul/src/main/java/ink/zhaibo/cat/zuul/CatZuulApplication.java
@@ -2,7 +2,9 @@ package ink.zhaibo.cat.zuul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
+@EnableZuulProxy
 @SpringBootApplication
 public class CatZuulApplication {
     public static void main(String[] args) {
